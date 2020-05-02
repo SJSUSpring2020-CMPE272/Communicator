7cd0c5d1a36f72d27cdd7972d9b8c4180aebc1cd
Salman Mal <salmanmal@yahoo.com>
2020-05-02 03:19:53 -0700

    node api

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-export const Node_API_URL='http://localhost:3001/';
+export const Node_API_URL='https://cmpe-272-sign-interpreter.herokuapp.com/';
 export const Python_API_URL='http://localhost:5000/';
